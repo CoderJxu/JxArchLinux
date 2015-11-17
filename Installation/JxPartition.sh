79a052730af02457a59319cff906985a88274209
Jerry Jianrui Xu <xujianrui@gmail.com>
2015-11-17 21:51:11 +1100

    Update JxPartition.sh

--- Installation/JxPartition.sh
+++ Installation/JxPartition.sh
@@ -3,7 +3,7 @@
 # Jxu @ 2015-11-16
 
 sgdisk --clear \
--a 4096
+-a 4096 \
 -n 1:0:+2M            -t 1:ef02 -c 1:"BIOS boot"  \
 -n 2:0:+300M          -t 2:8300 -c 2:"Linux boot" \
 -n 3:0:+50G           -t 3:8300 -c 3:"Linux root" \
