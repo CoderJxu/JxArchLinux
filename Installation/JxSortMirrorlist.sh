f54531fdfffc093e03c49774188434ba7aa3dfd4
Jerry Jianrui Xu <xujianrui@gmail.com>
2015-11-17 23:54:21 +1100

    Update JxSortMirrorlist.sh

--- Installation/JxSortMirrorlist.sh
+++ Installation/JxSortMirrorlist.sh
@@ -2,4 +2,4 @@
 
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
-rankmirrors -tv -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
